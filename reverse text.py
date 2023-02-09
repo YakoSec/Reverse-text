8a663f28d65ed773adad5ae8de2f70aebbefa36d
YakoSec <93792838+YakoSec@users.noreply.github.com>
2023-02-09 17:26:51 +0330

    Create reverse text.py

---
+++ reverse text.py
@@ -0,0 +1,15 @@
+# Coder = Mr. Yako
+# Instagram: @YakoSec
+# GitHub: @YakoSec
+# Telegram: @YakoSec
+
+import colorama
+from colorama import Fore,Back, Style
+colorama.init()
+
+print(Fore.GREEN)
+print("Coder: Mr. Yako \nInstagram: @YakoSec \nTelegram: @YakoSec \nGitHub: @YakoSec")
+
+print(Fore.YELLOW)
+text = input("Enter the text you want to see reverse: ")
+print("The reverse of the text you entered: ",text[::-1])
